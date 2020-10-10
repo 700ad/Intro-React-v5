c62744e7474e567a739b882417732c93d7939a4a
Adeel Farzand <adeel.farzand.1@gmail.com>
2020-10-11 02:49:02 +0500

    Added Support Babel + Eslint Support For JSX React Errors.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import { render } from 'react-dom';
 import Pet from './Pet';
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adpot Me"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatail",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-  ]);
+  return (
+    <div>
+      <h1>"Adpot Me"</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese"></Pet>
+      <Pet name="Pepper" animal="Bird" breed="Cockatial"></Pet>
+      <Pet name="Doink" animal="Cat" breed="Mixed"></Pet>
+    </div>
+  )
 };
 render(React.createElement(App), document.getElementById("root"));
