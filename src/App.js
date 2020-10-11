3ac03d3f8fad98b3d243bd2626c03226200fe0b4
Adeel Farzand <adeel.farzand.1@gmail.com>
2020-10-11 17:33:29 +0500

    Added Reach Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { render } from 'react-dom';
+import {Link, Router} from '@reach/router';
 import SearchParams from './SearchParams';
+import Details from './Details';
 const App = () => {
   return (
     <div>
-      <h1>"Adpot Me"</h1>
-      <SearchParams/>
+    <header>
+      <Link to="/">Adopt Me </Link>
+    </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   )
 };
