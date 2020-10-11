2d22995b439b5ddc1c80a402456af2ccbd1cd4b9
Adeel Farzand <adeel.farzand.1@gmail.com>
2020-10-11 11:40:22 +0500

    :tada: Async Calls to Api - Developed Pet Search Component

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
+import Results from './Results';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -7,7 +8,16 @@ const SearchParams = () => {
     const [breeds, setBreeds] = useState([]);
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog",ANIMALS)
     const [breed, BreedDrodown, setBreed] = useDropdown("Breed","", breeds)
-    
+    const [pets, setPets] = useState([]);
+    async function requestPets(){
+        const {animals} = await pet.animals({
+            location,
+            breed,
+            type:animal
+        })
+
+        setPets(animals || [])
+    }
     useEffect(()=>{
         setBreeds([]);
         setBreed("");
@@ -20,7 +30,10 @@ const SearchParams = () => {
 
     return (
         <div className="search-params">
-            <form>
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                requestPets()
+                }}>
                 <label htmlFor="location">
                     Location
                     <input id="location" value={location} onChange={e => setLocation(e.target.value)} placeholder="Location"/>
@@ -29,6 +42,7 @@ const SearchParams = () => {
                 <BreedDrodown/>
                 <button>Submit</button>
             </form>
+            <Results pets={pets}/>
         </div>
     )
 }
